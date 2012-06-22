70c35bb063580c5706b5042906620640987ee5c5
Brian Corrigan <brian@agoragames.com>
2012-06-22 09:42:40 -0400

    Adds something or other

--- personal/genexp.el
+++ personal/genexp.el
@@ -11,6 +11,9 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; Get rid of auto-fill mode
+(auto-fill-mode -1)
+(remove-hook 'text-mode-hook #'turn-on-auto-fill)
 
 (set-frame-font "Inconsolata-g-14")
 
@@ -20,5 +23,19 @@
 
 (add-hook 'prog-mode-hook 'prelude-turn-off-whitespace t)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode t)
+
+;; When Finder asks Emacs to open a file, don't create a new frame for it
+(setq ns-pop-up-frames nil)
+
+;; For some reason which I cannot begin to fathom, Emacs' default behavior
+;; when a file is dragged from the Finder onto an Emacs frame is to insert
+;; the file's contents at the spot where the mouse button is released. It
+;; makes over 9000 times more sense just to open the file in a new buffer.
+(define-key global-map [ns-drag-file] 'ns-find-file)
+
+;; Integrate with the standard OS X trash directory
+(setq
+ delete-by-moving-to-trash t
+ trash-directory (expand-file-name "~/.Trash"))
 
 (server-start)
